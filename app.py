468a0d1885a1b77c2301b0d2435766c66fd1409c
C110156238 <102135194+C110156238@users.noreply.github.com>
2023-06-14 10:32:05 +0800

    app.py

--- app.py
+++ app.py
@@ -124,42 +124,6 @@ def welcome(event):
     message = TextSendMessage(text=f'{name}歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
         
-# @app.route("/", methods=['POST'])
-# def linebot():
-#     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
-#     try:
-#         json_data = json.loads(body)                         # json 格式化訊息內容
-#         access_token = 'ix+FBt4qAcThr9OfcHGJndzIg3JrwWF1IpNrcFs/lTt+wq97kUuCe+RcG8sfkizlfrgaUORkIq7OGBZJ5GiMXQZv2TA8y12UmfTkWapMplA0IYL5tVS5owaruUANUufeRAQlt2zjLmrTcp2gjK/2ugdB04t89/1O/w1cDnyilFU='                                    # 你的 Access Token
-#         secret = '85919715906634b7a5064bc0c3b9b8f5'                                          # 你的 Channel Secret
-#         line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-#         handler = WebhookHandler(secret)                     # 確認 secret 是否正確
-#         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
-#         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
-#         tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
-#         type = json_data['events'][0]['message']['type']     # 取得 LINE 收到的訊息類型
-#         # 判斷如果是文字
-#         if type=='text':
-#             msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
-#             reply = msg
-#         # 判斷如果是圖片
-#         elif type == 'image':
-#             msgID = json_data['events'][0]['message']['id']  # 取得訊息 id
-#             message_content = line_bot_api.get_message_content(msgID)  # 根據訊息 ID 取得訊息內容
-#             # 在同樣的資料夾中建立以訊息 ID 為檔名的 .jpg 檔案
-#             with open(f'{msgID}.jpg', 'wb') as fd:
-#                 fd.write(message_content.content)             # 以二進位的方式寫入檔案
-#             reply = '圖片儲存完成！'                             # 設定要回傳的訊息
-#         else:
-#             reply = '你傳的不是文字或圖片呦～'
-#         print(reply)
-#         line_bot_api.reply_message(tk,TextSendMessage(reply))  # 回傳訊息
-#     except:
-#         print(body)                                            # 如果發生錯誤，印出收到的內容
-#     return 'OK'                                                # 驗證 Webhook 使用，不能省略
-
-# if __name__ == "__main__":
-#     app.run()
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
